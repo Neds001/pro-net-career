310bf2501bd6caee99685b2f5b036741cd799b74
Neds001 <nedsmamitag001@gmail.com>
2024-04-21 18:34:04 +0800

    removed plugin on vite config

--- vite.config.js
+++ vite.config.js
@@ -2,47 +2,9 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
-
-const manifestForPlugin = {
-  registerType: 'prompt',
-  includeAssets: ['logo.png'],
-  manifest: {
-    name: "Professional Network Career",
-    short_name: "ProNetCareer",
-    description: "ProNetCareer is the world's largest professional network on the internet. You can use ProNetCareer to find the right job or internship, connect and strengthen professional relationships, and learn the skills you need to succeed in your career.",
-    icons: [
-      {
-        src: '/assets/images/logo.png',
-        sizes: '192x192',
-        type: 'image/png',
-        purpose: 'favicon'
-      },
-      {
-        src: '/assets/images/logo.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'favicon'
-      },
-      {
-        src: '/assets/images/logo.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable'
-      }
-    ],
-    theme_color: '#181818',
-    background_color: '#e0cc3b',
-    display: "standalone",
-    scope: '/',
-    start_url: "/",
-    orientation: 'portrait'
-  },
-};
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA(manifestForPlugin)
   ],
 });
